187f4a3edc3391bc566f9e8eeb6ebff3da581b13
Anas Mohammad <akeelabanu4400@gmail.com>
2025-02-26 22:16:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Lecturer", "Developer", "Designer", "Freelancer"],
+        strings: [ "Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Lecturer", "Developer", ],
+        strings: [ "Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -71,3 +71,4 @@ $(document).ready(function(){
         }
     });
 });
+
